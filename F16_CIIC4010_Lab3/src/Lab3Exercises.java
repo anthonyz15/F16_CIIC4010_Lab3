62c75c38590d90a6489f5bf1fd4862028a77640f
4015d6 <4015d6@parcha.ece.uprm.edu>
2016-08-31 15:05:55 -0400

    change name and size

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame(" Anthony ");
+		JFrame myFrame = new JFrame("Anthony");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocation(100,100);
 		myFrame.setSize(200,200);
